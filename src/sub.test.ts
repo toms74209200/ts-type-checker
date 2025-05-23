001f8f74070f0268222928687f8a4fb8dd3de0e1
toms74209200 <motomotomato@gmail.com>
2025-05-23 09:14:59 +0900

    :white_check_mark: Update test for subtype

--- src/sub.test.ts
+++ src/sub.test.ts
@@ -11,11 +11,26 @@ Deno.test("typecheck subtype", () => {
   expect(actual.tag).toBe("Number");
 });
 
+Deno.test("typecheck subtype with additional properties", () => {
+  const input = `
+      const f = (x: { foo: { bar: number } }) => x.foo.bar;
+      f({ foo: { bar: 1, baz: true } });`;
+  const actual = typecheck(parseSub(input), {});
+  expect(actual.tag).toBe("Number");
+});
+
 Deno.test("typecheck subtype invalid", () => {
   const input = `
       type F = () => { foo: number; bar: boolean };
       const f = (x: F) => x().bar;
       const g = () => ({ foo: 1 });
       f(g);`;
+  expect(() => typecheck(parseSub(input), {})).toThrow();
+});
+
+Deno.test("typecheck subtype invalid with missing properties", () => {
+  const input = `
+      const f = (x: { foo: { bar: number; baz: boolean } }) => x.foo.baz;
+      f({ foo: { bar: 1 } });`;
   expect(() => typecheck(parseSub(input), {})).toThrow();
 });
